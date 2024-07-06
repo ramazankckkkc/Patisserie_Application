528c83a0ae649b30afa4727b80f151a4c4a8a80c
Ramazan Kucukkoc <ramazankckkc4234@gmail.com>
2024-07-06 21:01:39 +0300

    .

--- DataAccess/Contexts/AppDbContext.cs
+++ DataAccess/Contexts/AppDbContext.cs
@@ -1,11 +1,13 @@
 ﻿using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System.Reflection;
 
 namespace DataAccess.Contexts
 {
     public class AppDbContext : DbContext
     {
+        protected IConfiguration Configuration { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Sale> Sales { get; set; }
@@ -15,6 +17,10 @@ namespace DataAccess.Contexts
         {
             modelBuilder.ApplyConfigurationsFromAssembly(assembly: Assembly.GetExecutingAssembly());
         }
-
+        public AppDbContext(DbContextOptions dbContextOptions, IConfiguration configuration)
+       : base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
     }
 }
